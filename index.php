b3955900b9ed2784dce7b94c4fdf0c4545629432
akhipo14 <akhipo14@gmail.com>
2023-01-22 12:39:03 +0700

    upload semua file website hayo

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link      rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <title>Hayo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <!-- header -->
+    <header>
+
+      <div class="nav" >
+        <input type="checkbox" name="" value="" id="check">
+        <label for="check">
+          <i class="material-icons" id="dehaze">dehaze</i>
+          <i class="material-icons" id="close">close</i>
+        </label>
+        <img src="img/logohayo.png" alt="">
+
+        <ul>
+          <li> <a href="#home">Home</a> </li>
+          <li> <a href="#about-us">About Us</a> </li>
+          <li> <a href="#testimoni">Testimoni</a> </li>
+          <li> <a href="#gallery">Team</a> </li>
+          <li> <a href="https://wa.me/message/5XGLOQQXEIX2A1" class="active">Contact</a> </li>
+        </ul>
+      </div>
+      <br>
+      <div class="header"id="home">
+
+      </div>
+
+    <div class="header">
+
+      <div class="row" >
+        <div class="col-1 kiri">
+          <h2>Obat Tradisional Minangkabau</h2>
+          <br>
+          <h3>(Ubek Sadah)</h3>
+          <br>
+          <div class="p">
+
+          <p>Menghangatkan badan, menghilangkan rasa sakit perut,
+          masuk angin, nyeri otot dan gatal-gatal.</p>
+        </div>
+          <div class="alamat">
+
+            <i class="fa-solid fa-location-dot"></i>
+            <p >Jl. Pasar Baru, Cupak Tangah, kec. Pauh, Kota Padang, Sumatera Barat.</p>
+          </div>
+          <br>
+          <a href="https://wa.me/message/5XGLOQQXEIX2A1">Pesan Sekarang</a>
+
+        </div>
+        <div class="col-2">
+            <img src="img/hayobotol5.jpg" alt="" class="gambar">
+        </div>
+        <div class="icon-whatsapp">
+            <a href="https://wa.me/message/5XGLOQQXEIX2A1"><i class="fa-brands fa-whatsapp"></i></a>
+        </div>
+      </div>
+    </div>
+
+  </header>
+  <!-- sosial media -->
+  <div class="sosial-media">
+
+    <div class="media-icon">
+        <a href="https://www.facebook.com/profile.php?id=100082278021895" class="icon"> <i class="fab fa-facebook"></i></a>
+        <a href="https://instagram.com/obathayo?igshid=YmMyMTA2M2Y=" class="icon"> <i class="fa-brands fa-square-instagram"></i></a>
+        <a href="mailto:obathayo@gmail.com" class="icon"> <i class="fa-solid fa-envelope"></i></a>
+        <a href="https://wa.me/message/5XGLOQQXEIX2A1" class="icon"><i class="fa-brands fa-square-whatsapp"></i></a>
+    </div>
+  </div>
+  <!-- About Us -->
+  <div class="about-us" id="about-us">
+    <div class="title">
+      <h2>About Us</h2>
+    </div>
+    <div class="row-2">
+      <div class="col-1 kiri">
+        <img src="img/produkhayo.jpeg" alt="" class="">
+      </div>
+      <div class="col-2">
+        <h3>Hayo ( Hangat Yo)</h3>
+        <p>Hayo adalah obat tradisional minang kabau, yang berasa dari Kota Pasaman, Sumatera Barat,
+          yang terbuat dari resep obat turun temurun yang berkhasiat untuk mengobati berbagai penyakit
+          seperti gatal-gatal, masuk angin, nyeri otot dan lain sebagainya. selain itu hayo juga bisa digunakan
+          untuk menghangatkan badan, mengobati hidung tersumbat dan sebagai minyak urut.
+        </p>
+        </div>
+    </div>
+    <div class="test" id="testimoni">
+    </div>
+    <br>
+  </div>
+  <!-- Testimoni -->
+  <div class="testimoni">
+    <div class="title">
+      <h2>Testimoni</h2>
+    </div>
+    <div class="row-3">
+      <!-- Swiper -->
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="img/testi1.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi2.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi3.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi4.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi5.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi6.jpeg" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="img/testi7.jpeg" alt="">
+          </div>
+        </div>
+        <div ><i class="fa-solid fa-location-dot"></i></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+    </div>
+    <div class="gall"id="gallery">
+  </div>
+
+  <!-- gallery team-->
+
+  </div>
+  <div class="gallery-team" >
+    <div class="title">
+      <h2>Team</h2>
+    </div>
+    <div class="column">
+      <div class="col-1">
+        <img src="img/elvia.jpg" alt="">
+        <h3>Elvia Nora</h3>
+        <p>CEO</p>
+      </div>
+      <div class="col-1">
+        <img src="img/putri.jpg" alt="">
+        <h3>Maywulandari Putri</h3>
+        <p>CFO</p>
+      </div>
+      <div class="col-1">
+        <img src="img/zaqy.jpg" alt="">
+        <h3>Zaqy</h3>
+        <p>Marketing Manager</p>
+      </div>
+      <div class="col-1">
+        <img src="img/azry.jpg" alt="">
+        <h3>Azry</h3>
+        <p>Social Media Manager</p>
+      </div>
+      <div class="col-5">
+        <img src="img/hekal.jpeg" alt="">
+        <h3>Rafli Haikal</h3>
+        <p>Designer & Web Developer</p>
+      </div>
+    </div>
+
+    <div class="title">
+      <h2>Gallery</h2>
+    </div>
+    <div class="gallery-slider">
+
+      <div class="slider">
+          <div class="imgslide fade">
+            <img src="img/glr1.jpeg" alt="">
+          </div>
+          <div class="imgslide fade">
+            <img src="img/glr22.jpg" alt="">
+          </div>
+          <div class="imgslide fade">
+            <img src="img/glr3.jpeg" alt="">
+          </div>
+          <div class="imgslide fade">
+            <img src="img/glr4.jpeg" alt="">
+          </div>
+          <div class="imgslide fade">
+            <img src="img/glr5.jpeg" alt="">
+          </div>
+          <div class="navigations">
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+       </div>
+
+      </div>
+      <!-- The dots/circles -->
+
+    </div>
+    <div style="text-align:center; margin-top:-50px;">
+      <span class="dot" onclick="currentSlide(1)"></span>
+      <span class="dot" onclick="currentSlide(2)"></span>
+      <span class="dot" onclick="currentSlide(3)"></span>
+      <span class="dot" onclick="currentSlide(4)"></span>
+      <span class="dot" onclick="currentSlide(5)"></span>
+    </div>
+  </div>
+
+  <!-- footer -->
+  <div class="footer">
+    <a href="#"><i class="fa-solid fa-copyright"></i> Raflihaikal - 2022</a>
+  </div>
+
+  </body>
+  <!-- Swiper JS -->
+   <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+   <!-- Initialize Swiper -->
+   <script>
+     var swiper = new Swiper(".mySwiper", {
+       slidesPerView: 'auto',
+       spaceBetween: 40,
+       centeredSlides: true,
+       grabcursor:true,
+       loop: true,
+       pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+       },
+       navigation: {
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
+       },
+     });
+
+
+  </script>
+
+  <script type="text/javascript">
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("imgslide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+  </script>
+
+  <script type="text/javascript">
+    ScrollReveal({
+      reset: false,
+      distance: '60px',
+      duration: 2000,
+      delay: 500
+    });
+    ScrollReveal().reveal('.title ',{delay:50, origin:'bottom'});
+    ScrollReveal().reveal('.kiri,.slider,.row-3',{delay:50, origin:'bottom'});
+    ScrollReveal().reveal('.col-2',{delay:50, origin:'bottom'});
+    ScrollReveal().reveal('.media-icon i,.column img',{delay:50, origin:'bottom', interval:100});
+    ScrollReveal().reveal('.column ',{delay:50, origin:'bottom', interval:200});
+
+  </script>
+</html>
